f8fdbdb9cd4775f3168b79c5731c6fdac92c07a4
Anuroop Joy <anuroop.joy@hrblock.com>
2021-10-05 14:26:51 +0530

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-airbnb-eslint'`, () => {
+  it('should have as title \'angular-airbnb-eslint\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-airbnb-eslint');
